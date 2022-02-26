326eb3307a60fa4f1e57bea4117fbb4eb052dfef
Gourob Saha <gourobsaha009@gmail.com>
2022-02-27 01:19:45 +0600

    first commit

---
+++ HelloPy.py
@@ -0,0 +1,4 @@
+#Hello World
+
+print("Hello World!")
+print("Oh my God! I'm doing python..")
---
+++ Variables.py
@@ -0,0 +1,8 @@
+# variable = a container for a value. Behaves as the value that it contains.
+# Datatype - String
+First_name = "Gourob"
+Last_name = "Saha"
+Full_name = First_name+" "+Last_name
+
+print("Hello "+Full_name)
+
