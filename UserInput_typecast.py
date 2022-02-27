ce2e4691f3b7a4dd0a2f71be8515eccbe26c34d4
Gourob Saha <gourobsaha009@gmail.com>
2022-02-27 11:12:06 +0600

    Fourth Commit

---
+++ UserInput_typecast.py
@@ -0,0 +1,27 @@
+# Type casting = convert a datatype of a value to another datatype
+# User input = take value/s from user to perform certain operation
+
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+height = input("How tall are you?: ")
+
+# We need to type cast age & height, otherwise these will remain string
+age = int(age)+1
+height = float(height)
+
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+"cm tall")
+
+
+# Another way of type casting
+
+name1 = input("What is your name?: ")
+age1 = int(input("What is your age?: "))
+height1 = float(input("How tall are you?: "))
+
+age1 = age1+1
+
+print("Hello "+name1)
+print("You are "+str(age1)+" years old")
+print("You are "+str(height1)+"cm tall")
