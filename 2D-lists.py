de72a69ea796779068ca7f838cbfd71dbb660e0a
Gourob Saha <gourobsaha009@gmail.com>
2022-03-03 23:22:27 +0600

    Ninth Commit

---
+++ 2D-lists.py
@@ -0,0 +1,9 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+print(food)
+print(food[1][2])
---
+++ lists.py
@@ -0,0 +1,16 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza","hamburger","hotdog","spaghetti","pudding"]
+
+print(food[0])
+print("")
+
+food.append("ice cream")
+food.remove("hotdog")
+food.pop()
+food.insert(0,"cake")
+food.sort()
+#food.clear()
+
+for x in food:
+    print(x)
