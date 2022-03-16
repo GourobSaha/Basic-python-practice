935f7d4b3cf76b8b525b9357e67f72979843bf22
Gourob Saha <gourobsaha009@gmail.com>
2022-03-16 19:19:30 +0600

    Eleventh Commit

---
+++ Python-sets.py
@@ -0,0 +1,19 @@
+# set = collection which is unordered, unindexed. No duplicate values
+
+utensils = {"fork","spoon","knife"}
+dishes = {"bowl","plate","cup","knife"}
+
+utensils.add("napkin")
+utensils.remove("fork")
+#utensils.clear()
+dishes.update(utensils)
+dinner_table = utensils.union(dishes)
+
+print(dishes.difference(utensils))
+print(utensils.intersection(dishes))
+
+for x in utensils:
+    print(x)
+
+for y in dishes:
+    print(y)
