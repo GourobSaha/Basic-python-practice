ec09165d488e3405bf17d7c71a977f1107b8e083
Gourob Saha <gourobsaha009@gmail.com>
2022-03-17 19:18:21 +0600

    14th Commit

---
+++ Python-functions.py
@@ -0,0 +1,9 @@
+#function = a block of code which is executed only when it is called.
+
+def hello(first_name,last_name,age):
+    print("Hello "+first_name+" "+last_name)
+    print("You are "+str(age+1)+" years old")
+    print("Have a nice day!")
+
+hello("Gourob","Saha",21)
+hello("Mizan","Masum",22)
