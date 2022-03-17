ed917e323b934abd884a922860706fad97f111b9
Gourob Saha <gourobsaha009@gmail.com>
2022-03-17 19:24:08 +0600

    15th Commit

--- Python-functions.py
+++ Python-functions.py
@@ -6,4 +6,15 @@ def hello(first_name,last_name,age):
     print("Have a nice day!")
 
 hello("Gourob","Saha",21)
-hello("Mizan","Masum",22)
+#hello("Mizan","Masum",22)
+
+
+#return statement = Functions send Python values/objects back to the caller.
+#                    These values/objects are known as the function’s return value
+
+def multiply(number1,number2):
+    return number1 * number2
+
+x = multiply(6,8)
+
+print("Your lucky number is "+str(x))
