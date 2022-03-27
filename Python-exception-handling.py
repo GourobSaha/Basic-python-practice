73bba98d3c5f4c31ddccb4c1e2d83e98d62f48cc
Gourob Saha <gourobsaha009@gmail.com>
2022-03-27 19:27:50 +0600

    24th Commit

---
+++ Python-exception-handling.py
@@ -0,0 +1,19 @@
+# exception =   events detected during execution that interrupt the flow of a program
+
+try:
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+except ZeroDivisionError as e:
+    print(e)
+    print("You can't divide by zero!")
+except ValueError as e:
+    print(e)
+    print("Enter only numbers plz")
+except Exception as e:
+    print(e)
+    print("something went wrong :(")
+else:
+    print(result)
+finally:
+    print("This will always execute")
