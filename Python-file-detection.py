4a238f30d3147bbfa16db225f2570b5b6c86a365
Gourob Saha <gourobsaha009@gmail.com>
2022-03-27 19:31:01 +0600

    25th Commit

---
+++ Python-file-detection.py
@@ -0,0 +1,13 @@
+
+import os
+
+path = "C:\\Users\\Admin\\Desktop\\test.txt"
+
+if os.path.exists(path):
+    print("That location exists!")
+    if os.path.isfile(path):
+        print("That is a file")
+    elif os.path.isdir(path):
+        print("That is a directory!")
+else:
+    print("That location doesn't exist!")
