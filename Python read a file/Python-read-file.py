ef68fa7fa58b8431cebd708e33dfcecd83fc1dde
Gourob Saha <gourobsaha009@gmail.com>
2022-03-28 21:43:54 +0600

    27th Commit

--- Python read a file/Python-read-file.py
+++ Python read a file/Python-read-file.py
@@ -1,5 +1,12 @@
+# Read a file
 try:
     with open('test.txt') as file:
         print(file.read())
 except FileNotFoundError:
     print("That file was not found :(")
+
+# Write a file
+text = "Yooooooooo\nThis is some text\nHave a good one!\n"
+
+with open('test.txt','w') as file:
+    file.write(text)
