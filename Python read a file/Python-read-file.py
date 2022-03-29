ec7c63daaf00c47ad6a316be6749c7e88fb2a51c
Gourob Saha <gourobsaha009@gmail.com>
2022-03-29 20:09:32 +0600

    26th Commit

--- Python read a file/Python-read-file.py
+++ Python read a file/Python-read-file.py
@@ -10,3 +10,12 @@ text = "Yooooooooo\nThis is some text\nHave a good one!\n"
 
 with open('test.txt','w') as file:
     file.write(text)
+
+
+# copyfile() =  copies contents of a file
+# copy() =      copyfile() + permission mode + destination can be a directory
+# copy2() =     copy() + copies metadata (file’s creation and modification times)
+
+import shutil
+
+shutil.copyfile('test.txt','copy.txt') #src,dst
