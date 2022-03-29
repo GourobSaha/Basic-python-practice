5952f263d73dbcd43edad36efee6cbd51ef23b4f
Gourob Saha <gourobsaha009@gmail.com>
2022-03-29 20:22:02 +0600

    29th Commit

--- Python read a file/Python-read-file.py
+++ Python read a file/Python-read-file.py
@@ -35,3 +35,23 @@ try:
         print(source+" was moved")
 except FileNotFoundError:
     print(source+" was not found")
+
+
+# Remove a file or a directory
+import os
+import shutil
+
+path = "test.txt"
+
+try:
+    os.remove(path)        #delete a file
+    #os.rmdir(path)        #delete an empty directory
+    #shutil.rmtree(path)   #delete a directory containing files
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to delete that")
+except OSError:
+    print("You cannot delete that using that function")
+else:
+    print(path+" was deleted")
