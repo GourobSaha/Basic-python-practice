45adcc4274b86b98843a8971687f5fceec06ff19
Gourob Saha <gourobsaha009@gmail.com>
2022-03-30 20:27:12 +0600

    31st Commit

---
+++ car.py
@@ -0,0 +1,13 @@
+class Car:
+
+    def __init__(self,make,model,year,color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def drive(self):
+        print("This "+self.model+" is driving")
+
+    def stop(self):
+        print("This "+self.model+" is stopped")
---
+++ oop1.py
@@ -0,0 +1,12 @@
+from car import Car
+
+car_1 = Car("Chevy","Corvette",2021,"blue")
+car_2 = Car("Ford","Mustang",2022,"red")
+
+print(car_1.model)
+print(car_2.make)
+print(car_1.year)
+print(car_2.color)
+
+car_1.drive()
+car_2.stop()
